d5ff5e5fa2e0a31dd62cc068b9540a3d51b06a9f
schen@superv.com.cn <schen@superv.com.cn>
2020-12-18 16:50:18 +0800

    1111

---
+++ src/main/java/com/schen/App.java
@@ -0,0 +1,13 @@
+package com.schen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/schen/demo/RabbitConsumer.java
@@ -0,0 +1,44 @@
+package com.schen.demo;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitConsumer {
+
+    private static final String QUEUE_NAME = "queue_demo";
+
+    private static final String IP_ADDRESS = "192.168.71.128";
+
+    private static final Integer PORT = 5672;
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Address[] adresses = new Address[]{
+                new Address(IP_ADDRESS,PORT)
+        };
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("root");
+        factory.setPassword("root");
+        Connection connection = factory.newConnection(adresses);
+        final Channel channel = connection.createChannel();
+        channel.basicQos(64);
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("recv message "+ new String(body));
+                try{
+                    TimeUnit.SECONDS.sleep(1);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                super.handleDelivery(consumerTag, envelope, properties, body);
+            }
+        };
+        channel.basicConsume(QUEUE_NAME,consumer);
+        // 等待回调函数执行完毕之后，关闭资源
+        channel.close();
+        connection.close();
+    }
+}
